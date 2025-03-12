06ac639e63de39799cced93737fe6992d472054f
Will K <20567256+Wkemp@users.noreply.github.com>
2025-03-12 12:49:51 -0500

    Update HelloWorldExample.java

--- server/src/main/java/com/example/HelloWorldExample.java
+++ server/src/main/java/com/example/HelloWorldExample.java
@@ -7,9 +7,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/hello") //Annotation-based mapping
+         
 public class HelloWorldExample extends HttpServlet {
          private static final long serialVersionUID = 1L;
 
+         @Override
          protected void doGet(HttpServletRequest request, HttpServlet response)
                            throws ServletException, IOException {
                   response.setContentType("text/html");
