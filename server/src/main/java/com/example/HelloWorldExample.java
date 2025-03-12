339217cd17a991c8ff771c1704c5487295100f2e
Will K <20567256+Wkemp@users.noreply.github.com>
2025-03-12 12:46:45 -0500

    Update HelloWorldExample.java

--- server/src/main/java/com/example/HelloWorldExample.java
+++ server/src/main/java/com/example/HelloWorldExample.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorldExample extends HttpServlet {
          private static final long serialVersionUID = 1L;
 
-         @Override
          protected void doGet(HttpServletRequest request, HttpServlet response)
                            throws ServletException, IOException {
                   response.setContentType("text/html");
