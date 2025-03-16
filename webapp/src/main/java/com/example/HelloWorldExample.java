aabd66710cef152a78a748922a27badc81a8c4ad
Will K <20567256+Wkemp@users.noreply.github.com>
2025-03-16 10:46:23 -0500

    Update and rename HelloWorldExample.java to HelloWorldExample.java

--- webapp/src/main/java/com/example/HelloWorldExample.java
+++ webapp/src/main/java/com/example/HelloWorldExample.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/hello") // Annotation-based mapping
          
 public class HelloWorldExample extends HttpServlet {
          private static final long serialVersionUID = 1L;
